3d43afbb031242da41139ae4b105078f75729117
hiroo <hirorororo0329@gmail.com>
2023-03-15 02:11:42 -0400

    before the meeting

--- src/MAPregression.m
+++ src/MAPregression.m
@@ -17,33 +17,79 @@ elseif type == "offset"
 elseif type == "normal"
     % y = meanFR + noise
     Y = firingRates;
+    X = tuningCurves;
+elseif type == "both"
+    % y = a(RT + B) + c(RT + D)*meanFR + noise
+    Y = firingRates - tuningCurves;
+    X = cat(2, reactionTimes.*tuningCurves, tuningCurves, reactionTimes, ones(ntrials, 1));
 end
 
-
-if type ~= "normal"
-    if regression == "ML"
-        % ML estimate
-        w = inv(X' * X) * X' * Y;
-    elseif regression == "MAP"
-        % MAP estimate (ridge regression)
-        lambda = 0.1;
-        w = inv(X' * X + lambda * eye(size(X, 2))) * X' * Y;
+% split data into training and test sets 
+% k-fold cross validation
+k = 10;
+n = size(X, 1);
+indices = crossvalind('Kfold', n, k);
+for i = 1:k
+    test = (indices == i); train = ~test;
+    Xtrain = X(train, :);
+    Ytrain = Y(train);
+    Xtest = X(test, :);
+    Ytest = Y(test);
+    % get model parameters
+    if type ~= "normal"
+        if regression == "ML"
+            % ML estimate
+            w = inv(Xtrain' * Xtrain) * Xtrain' * Ytrain;
+        elseif regression == "MAP"
+            % MAP estimate (ridge regression)
+            lambda = 0.1;
+            w = inv(Xtrain' * Xtrain + lambda * eye(size(Xtrain, 2))) * Xtrain' * Ytrain;
+        end
+    else
+        w = nan;
     end
-else
-    w = nan;
+    % get root mean squared error and r2 value
+    if type == "normal"
+        SEs = (Ytest-tuningCurves(test)).^2;
+        RMSE(i) = sqrt(mean(SEs));
+        r2(i) = 1 - (sum((Ytest - tuningCurves(test)).^2) / sum((Ytest - mean(Ytest)).^2));
+        AIC(i) = nan;
+    else
+        SEs = (Ytest - Xtest * w).^2;
+        RMSE(i) = sqrt(mean(SEs));
+        r2(i) = 1 - (sum((Ytest - Xtest * w).^2) / sum((Ytest - mean(Ytest)).^2));
+        % get AIC
+        AIC(i) = ntrials * log(RMSE(i)) + 2 * length(w);
+    end
 end
 
-% get root mean squared error and r2 value
-if type == "normal"
-    RMSE = sqrt(mean((Y - tuningCurves).^2));
-    r2 = 1 - (sum((Y - tuningCurves).^2) / sum((Y - mean(Y)).^2));
-    AIC = nan;
-else
-    RMSE = sqrt(mean((Y - X * w).^2));
-    r2 = 1 - (sum((Y - X * w).^2) / sum((Y - mean(Y)).^2));
-    % get AIC
-    AIC = ntrials * log(RMSE) + 2 * length(w);
-end
+
+% if type ~= "normal"
+%     if regression == "ML"
+%         % ML estimate
+%         w = inv(X' * X) * X' * Y;
+%     elseif regression == "MAP"
+%         % MAP estimate (ridge regression)
+%         lambda = 0.1;
+%         w = inv(X' * X + lambda * eye(size(X, 2))) * X' * Y;
+%     end
+% else
+%     w = nan;
+% end
+
+% % get root mean squared error and r2 value
+% if type == "normal"
+%     SEs = (Y-tuningCurves).^2;
+%     RMSE = sqrt(mean(SEs));
+%     r2 = 1 - (sum((Y - tuningCurves).^2) / sum((Y - mean(Y)).^2));
+%     AIC = nan;
+% else
+%     SEs = (Y - X * w).^2;
+%     RMSE = sqrt(mean(SEs));
+%     r2 = 1 - (sum((Y - X * w).^2) / sum((Y - mean(Y)).^2));
+%     % get AIC
+%     AIC = ntrials * log(RMSE) + 2 * length(w);
+% end
 
 
 end
--- src/compareRegression.m
+++ src/compareRegression.m
@@ -1,4 +1,4 @@
-clear all; close all;
+ clear all; close all;
 % This is the main file
 % Fit the spike trains into 4 different model
 % 1. gain model
@@ -41,19 +41,23 @@ rewards = unique(rewardLabels); nrewards = length(rewards);
 
 %% get the tuning curve in each reward condition
 % output: 
-% meanFR: nrewards * ndirections (double)
+% meanFR: nrewards * ndirections * nneurons(double)
 % tuningCurves: ntrials * 1
-meanFR = zeros(nrewards, ndirections);
-for i = 1:nrewards
-    for j = 1:ndirections
-        curInds = rewardLabels == i & directionLabels == j;
-        meanFR(i, j) = mean(firingRates(curInds));
+meanFR = zeros(nneurons, nrewards, ndirections);
+for i = 1:nneurons
+    for j = 1:nrewards
+        for k = 1:ndirections
+            curInds = rewardLabels == j & directionLabels == k;
+            meanFR(i, j, k) = mean(firingRates(curInds, i));
+        end
     end
 end
 
-tuningCurves = zeros(ntrials, 1);
-for i = 1:ntrials
-    tuningCurves(i) = meanFR(rewardLabels(i), directionLabels(i));
+tuningCurves = zeros(ntrials, nneurons);
+for i = 1:nneurons
+    for j = 1:ntrials
+        tuningCurves(j, i) = meanFR(i, rewardLabels(j), directionLabels(j));
+    end
 end
 
 %% in each neurons, firing rates is fitted to 3 different models
@@ -68,32 +72,125 @@ end
 % AIC/BIC: double
 
 %% plot 3 * 2 different model and regression
-types = ["gain", "offset", "normal"];
+types = ["both", "gain", "offset", "normal"];
 regressions = ["ML", "MAP"];
+ntypes = length(types);
+nregressions = length(regressions);
 
+Yall = zeros(nneurons, ntypes, nregressions);
+Pall = zeros(nneurons, 1);
+Pall2 = zeros(nneurons, 1);
 for n=1:nneurons
-    Y = zeros(length(regressions), length(types));
-    for i = 1:length(regressions)
+    Y = cell(1, ntypes);
+    for i = 1:1
         regression = regressions(i);
-        for j = 1:length(types)
+        for j = 1:ntypes
             type = types(j);
-            [w, RMSE, r2, AIC] = MAPregression(firingRates(:, n), tuningCurves, reactionTimes, rewardLabels, directionLabels, type, regression);
-            Y(i, j) = RMSE;
+            [w, RMSE, r2, AIC] = MAPregression(firingRates(:, n), tuningCurves(:, n), reactionTimes, rewardLabels, directionLabels, type, regression);
+            Y{i, j} = RMSE;
+            Yall(n, j, i) = mean(RMSE);
         end
     end
-%     figure;
-%     for i = 1:length(regressions)
-%         subplot(1,2,i);
-%         bar(1:3, Y(i, :));
-%         xticks([1 2 3])
-%         xticklabels(types);
-%         ylabel("root mean squared error (Hz)")
-%         title(regression);
-%     end
-%     if status == 0
-%         n = "fake";
-%     end
-%     saveas(gcf, "../results/20230310/neuron-"+n+".jpg");
-%     close all;
+    figure;
+    for i = 1:1
+        % subplot(1,2,i);
+        % plot bar and error bar
+        y = zeros(1, ntypes);
+        yerr = zeros(1, ntypes);
+        for j = 1:ntypes
+            y(j) = mean(Y{i, j});
+            yerr(j) = std(Y{i, j}) / sqrt(10); % k=10
+        end
+        bar(1:ntypes, y(i, :));
+        hold on;
+        errorbar(1:ntypes, y(i, :), yerr(i, :), 'LineStyle', 'none');
+        xticks(1:ntypes)
+        xticklabels(types);
+        ylabel("root mean square error (Hz)")
+        % get p-value between 2 and 3 type
+        [~, p] = ttest2(Y{i, 2}, Y{i, 3});
+        Pall(n) = p;
+        title(regression + " p-value: " + p);
+        % title(regression);
+
+        [~, p] = ttest2(Y{i, 2}, Y{i, 1});
+        Pall2(n) = p;
+    end
+    if status == 0
+        n = "fake";
+    end
+    set(gca, 'fontsize', 14, 'fontname', 'arial', 'tickdir', 'out');
+    set(gcf,'position',[100,100,300,650]);
+    saveas(gcf, "../results/20230314/neuron-"+n+".jpg");
+    close all;
 end
+
+%% plot the mean of all neurons
+Y = zeros(1, ntypes);
+Yerr = zeros(1, ntypes);
+for i = 1:1
+    regression = regressions(i);
+    for j = 1:ntypes
+        type = types(j);
+        Y(i, j) = mean(Yall(:, j, i));
+        Yerr(i, j) = std(Yall(:, j, i)) / sqrt(nneurons);
+    end
+end
+figure;
+for i = 1:1
+    % subplot(1,2,i);
+    % plot bar and error bar
+    bar(1:ntypes, Y(i, :));
+    hold on;
+    errorbar(1:ntypes, Y(i, :), Yerr(i, :), 'LineStyle', 'none');
+    xticks(1:ntypes)
+    xticklabels(types);
+    ylabel("root mean square error (Hz)")
+    % get p-value between 2 and 3 type
+    [~, p] = ttest2(squeeze(Yall(:, 2, i)), squeeze(Yall(:, 3, i)));
+    title(regression + " p-value: " + p);
+    % title(regression);
+end
+set(gca, 'fontsize', 14, 'fontname', 'arial', 'tickdir', 'out');
+set(gcf,'position',[100,100,300,650]);
+saveas(gcf, "../results/20230314/neuron-all.jpg");
+close all;
+
+% count the number of neurons that 
+% class 1: p < 0.05 and the Yall(:, 2, 1) < Yall(:, 3, 1)
+% class 2: p < 0.05 and the Yall(:, 2, 1) > Yall(:, 3, 1)
+% class 3: p > 0.05 and the Yall(:, 2, 1) < Yall(:, 3, 1)
+% class 4: p > 0.05 and the Yall(:, 2, 1) > Yall(:, 3, 1)
+class1 = sum(Pall < 0.05 & Yall(:, 2, 1) < Yall(:, 3, 1));
+class2 = sum(Pall < 0.05 & Yall(:, 2, 1) > Yall(:, 3, 1));
+class3 = sum(Pall > 0.05 & Yall(:, 2, 1) < Yall(:, 3, 1));
+class4 = sum(Pall > 0.05 & Yall(:, 2, 1) > Yall(:, 3, 1));
+
+% plot the counts in each class in a bar plot
+figure;
+bar([class1, class2, class3, class4]);
+xticks(1:4)
+xticklabels(["Gain", "Offset", "Nosig G", "Nosig O"]);
+ylabel("Number of neurons")
+set(gca, 'fontsize', 14, 'fontname', 'arial', 'tickdir', 'out');
+set(gcf,'position',[100,100,300,650]);
+saveas(gcf, "../results/20230314/neuron-class.jpg");
+close all;
+
+
+% count the number of neurons that
+% class 1 Pall2 < 0.05
+% class 2 Pall2 > 0.05
+
+class1 = sum(Pall2 < 0.05);
+class2 = sum(Pall2 > 0.05);
+figure;
+bar([class1, class2]);
+xticks(1:2)
+xticklabels(["Sig", "Nosig"]);
+ylabel("Number of neurons")
+set(gca, 'fontsize', 14, 'fontname', 'arial', 'tickdir', 'out');
+set(gcf,'position',[100,100,300,650]);
+saveas(gcf, "../results/20230314/neuron-class2.jpg");
+close all;
 
